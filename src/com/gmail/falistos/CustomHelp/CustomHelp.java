b3a4ed31a7d7a8c0fae1ef575260634cde87b35c
Falistos <falistos@gmail.com>
2015-03-29 23:43:23 +0200

    Initial project commit

---
+++ src/com/gmail/falistos/CustomHelp/CustomHelp.java
@@ -0,0 +1,60 @@
+package com.gmail.falistos.CustomHelp;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CustomHelp extends JavaPlugin implements Listener {
+
+	public void onEnable() {
+		this.getConfig().options().copyDefaults(true);
+		this.saveDefaultConfig();
+		
+		this.getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("help")) {
+			if (args.length > 0 && args[0].equalsIgnoreCase("reload")) {
+				if (!sender.hasPermission("customhelp.reload")) {
+					sender.sendMessage(ChatColor.RED+"You are not allowed to do this.");
+					return true;
+				}
+				
+				this.reloadConfig();
+				sender.sendMessage(ChatColor.GREEN+this.getName()+" "+this.getDescription().getVersion()+" reloaded.");
+				return true;
+			}
+			
+			int page = 0;
+			
+			if (args.length >= 1) {
+				try {
+			        page = Integer.parseInt(args[0]) - 1; 
+			    } catch(NumberFormatException e) { 
+			    	page = 0;
+			    }
+			}
+			
+			ArrayList<String> list = new ArrayList<String>();
+			Map<String, Object> commandsMap = this.getConfig().getConfigurationSection("commands").getValues(true);
+			
+			for (Entry<String, Object> entry : commandsMap.entrySet()) {
+				list.add(ChatColor.GOLD+entry.getKey()+ChatColor.WHITE+": "+entry.getValue());
+			}
+			
+			new PaginatedCommandsMessageList(this.getConfig().getString("header"), this.getConfig().getString("footer")).send(sender, list, page);
+			
+			return true;
+		}
+		
+		return false;
+	}
+}
---
+++ src/com/gmail/falistos/CustomHelp/PaginatedCommandsMessageList.java
@@ -0,0 +1,60 @@
+package com.gmail.falistos.CustomHelp;
+
+import java.util.ArrayList;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+public class PaginatedCommandsMessageList {
+
+	private String header = "";
+	private String footer = "";
+	
+	private int entriesPerPage = 8;
+	
+    public PaginatedCommandsMessageList(String header, String footer, int entriesPerPage) {
+    	this.setHeader(header);
+    	this.setFooter(footer);
+    	this.setEntriesPerPage(entriesPerPage);
+    }
+    
+    public PaginatedCommandsMessageList(String header, String footer) {
+    	this.setHeader(header);
+    	this.setFooter(footer);
+    }
+	
+    public PaginatedCommandsMessageList(int entriesPerPage) {
+    	this.setEntriesPerPage(entriesPerPage);
+    }
+    
+    public PaginatedCommandsMessageList() { }
+    
+    public void setHeader(String header) { 
+    	this.header = header;
+    }
+    
+    public void setFooter(String footer) {
+    	this.footer = footer;
+    }
+    
+    public void setEntriesPerPage(int entriesPerPage) {
+    	this.entriesPerPage = entriesPerPage;
+    }
+    
+    public void send(CommandSender sender, ArrayList<String> list, int page) {
+    	if (list.size() == 0) return;
+    	
+    	int pages = list.size() / this.entriesPerPage;
+    	if (list.size() % this.entriesPerPage == 0) pages--;
+    	
+    	page = Math.max(0, Math.min(page, pages));
+        
+    	sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.header.replace("{page}", ""+page+1).replace("{maxpages}", ""+pages+1).replace("{nextpage}", ""+page+2)));
+        
+        for (int i = this.entriesPerPage * page; i < this.entriesPerPage * page + this.entriesPerPage  && i < list.size(); i++) {
+            sender.sendMessage(list.get(i));
+        }
+        
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.footer.replace("{page}", ""+page+1).replace("{maxpages}", ""+pages+1).replace("{nextpage}", ""+page+2)));
+	}
+}
